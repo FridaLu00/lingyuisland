a74b4d9b2be7525bdb7d5bb30481f93df3c044c9
FridaLu00 <luyitong00@gmail.com>
2026-05-03 13:08:30 +0800

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ['*.dev.coze.site'],
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: '*', pathname: '/**' }],
+  },
+};
+
+export default nextConfig;
